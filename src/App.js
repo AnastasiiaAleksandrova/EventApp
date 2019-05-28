f00620b3c98da8c5a21d6b84433062762e94a621
Petrenko Rostislav <s1900169@edu.bc.fi>
2019-05-28 11:27:28 +0300

    html. Still in process

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           Yes it is a pull, Go check laa
-        </a>
+      <header>
+        <h1>Events map</h1>
+      {/*
+        <div>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>*/}
       </header>
+
+      <div className="menu">
+        <button>Category</button>
+        <button>Category</button>
+        <button>Category</button>
+        <button>Category</button>
+      </div>
+      <main>
+        <div className="side-events">
+        <div>
+          I am image
+        </div>
+          I am image
+        </div>
+        <div className="map-events">
+          i am map
+        </div>
+      </main>
+
+      <footer>
+        i am footer
+      </footer>
     </div>
   );
 }
