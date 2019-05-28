09511594049fae3932f028eb231511211c1d9384
ValentinaJakobson <jakobsonvv@gmail.com>
2019-05-28 11:33:37 +0300

    map first version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Map from './Map/Map';
+import AnyReactComponent from './Map/Map';
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <Map />
+
     </div>
   );
 }
