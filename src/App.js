a538fb08a7dd5a7fdfb143cbcd3806d4eb82a09a
xlakostax <konstantin.veselovskii@gmail.com>
2019-05-29 11:22:07 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import React from 'react';
 import './App.css';
 import Map from './Map/Map';
 
-
-
-
-
-
-
-
-
 function App() {
   return (
     <div className="App">
-
       <header>
         <h1>Events map</h1>
       </header>
@@ -25,7 +16,7 @@ function App() {
             <li><input type="checkbox" name="vehicle" value="1" />test</li>
             <li><input type="checkbox" name="vehicle" value="1" />test</li>
           </ul>
-          </div>
+        </div>
         <div className="filter">
           <button>date <i class="fas fa-angle-down"></i></button>
           <ul>
@@ -33,7 +24,7 @@ function App() {
             <li><input type="checkbox" name="vehicle" value="1" />test</li>
             <li><input type="checkbox" name="vehicle" value="1" />test</li>
           </ul>
-          </div>
+        </div>
         <div className="filter">
           <button>smth <i class="fas fa-angle-down"></i></button>
           <ul>
@@ -41,34 +32,32 @@ function App() {
             <li><input type="checkbox" name="#" value="1" />test</li>
             <li><input type="checkbox" name="#" value="1" />test</li>
           </ul>
-          </div>
+        </div>
         <div className="filter">
-        <button>smth <i class="fas fa-angle-down"></i></button>
-        <ul>
-          <li><input type="checkbox" name="#" value="1" />test</li>
-          <li><input type="checkbox" name="#" value="1" />test</li>
-          <li><input type="checkbox" name="#" value="1" />test</li>
-        </ul>
+          <button>smth <i class="fas fa-angle-down"></i></button>
+          <ul>
+            <li><input type="checkbox" name="#" value="1" />test</li>
+            <li><input type="checkbox" name="#" value="1" />test</li>
+            <li><input type="checkbox" name="#" value="1" />test</li>
+          </ul>
         </div>
       </div>
-
       <main>
         <div className="side-events">
-        <div>
-          I am image
-        </div>
-          I am image
+          <div>
+            I am image
+          </div>
+          <div>
+            I am image
+          </div>
         </div>
         <div className="map-events">
           <Map />
         </div>
       </main>
-
       <footer>
         i am footer
       </footer>
-
-
     </div>
   );
 }
