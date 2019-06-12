43e0ee7f32f721246455a85131d23a6f3a4ddcf5
xlakostax <konstantin.veselovskii@gmail.com>
2019-06-12 16:47:25 +0300

    sf

--- src/RadioInput.js
+++ src/RadioInput.js
@@ -4,7 +4,7 @@ import React from 'react';
 function RadioInput(props) {
 
     return (
-        <input className="RadioInput" type="radio" name={props.name} value={props.value} onChange={props.onChange}></input>
+        <input className="radio-input" type="radio" name={props.name} value={props.value} onChange={props.onChange}></input>
     );
   }
 
