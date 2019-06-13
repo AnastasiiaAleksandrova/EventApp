2653f8d9d6ddd08401ea4ba13d74ef22cd7b3685
ValentinaJakobson <jakobsonvv@gmail.com>
2019-06-13 23:33:07 +0300

    smth

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,106 +1,63 @@
-// import React, { Component } from 'react';
-// import GoogleMapReact from 'google-map-react';
-// import Marker from './Marker/Marker';
-
-
-// class Map extends Component {
-//  static defaultProps = {
-//    center: {
-//     lat: 60.1847984,
-//     lng: 24.9389162,
-//    },
-//    zoom: 13,
-//  };
-//  constructor (props){
-//    super(props)
-//    this.state = {
-//     myLat: null,
-//     myLon: null,
-//     showingInfoWindow: false,
-//       activeMarker: {},
-//       selectedPlace: {},
-//       streetViewControl: true
-//    }
-//  };
-
-//  componentDidMount(){
-//    navigator.geolocation.getCurrentPosition((position) => {
-//      this.setState({
-//        myLat:position.coords.latitude,
-//        myLon:position.coords.longitude
-//      });
-// console.log(position);
-//    });
-//  };
-
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import './Map.css';
+import Marker from './Marker/Marker';
 
 class Map extends Component {
- static defaultProps = {
-   center: {
-    lat: 60.1847984,
-    lng: 24.9389162,
-   },
-   zoom: 13,
+  static defaultProps = {
+    center: {
+      lat: 60.2019475,
+      lng: 24.9286974
+    },
+    zoom: 12,
+  };
 
-// onClose = props => {
-//    if (this.state.showInfoWindow) {
-//      this.setState({
-//        showInfoWindow: false,
-//      });
-//    }
-//  };
-
-//  onMapClick = (props) => {
-//   if (this.state.showingInfoWindow) {
-//     this.setState({
-//       showingInfoWindow: false,
-//       activeMarker: null
-//     });
-//   }
-// }
- 
-createMapOptions(maps) {
-  return {
-    streetViewControl: true,
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        myLat:position.coords.latitude,
+        myLon:position.coords.longitude
+      });
+    });
   }
-} 
 
-// createMapOptions(maps) {
-//   return {
-//     streetViewControl: true,
-//   }
-// } 
+  onMarkerClick = props =>
+    this.setState({
+      showInfoWindow: true
+    });
 
+  onClose = props => {
+    if (this.state.showInfoWindow) {
+      this.setState({
+        showInfoWindow: false,
+      });
+    }
+  };
 
-//  render() {
-//    return (
-//      <div style={{position: 'relative', height: '94vh', width: '100%'}} >
-//        <GoogleMapReact
-//          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY}}
-//          defaultCenter={this.props.center}
-//          defaultZoom={this.props.zoom}
-//          options={this.createMapOptions}
-//        >
-//        {this.props.events.map((el, index) => {
-//          return(
-//          <Marker
-//             key={index}
-//             lat={el.lat}
-//             lng={el.lon}
-//             onClick={this.onMarkerClick} />
-//          )
-//        } )
-//  }
+  render() {
+    return (
+      <div className='map-wrapper' style={this.props.style}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          options={this.createMapOptions}
+        >
+        {this.props.events.map((el, index) => {
+          return(
+            <Marker
+              key={index}
+              lat={el.lat}
+              lng={el.lon}
+              text='ХУЙ!'
+              onClick={this.onMarkerClick}
+            />
+          )
+        })}
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
 
-//      <Marker
-//       lat={this.state.myLat}
-//       lng={this.state.myLon}
-//        />
-//     </GoogleMapReact>
-//   </div>
-//    );
-//  }
-// };
-
-
-// export default Map;
+export default Map;
