4fac6e308122eb65bd849667417ba441952a88bf
xlakostax <konstantin.veselovskii@gmail.com>
2019-06-14 13:17:00 +0300

    sdf

---
+++ src/Map/Marker/Marker.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import  Pin  from '../pin.svg';
+
+let Marker = ({ text }) => {
+  return (
+    <div style={{
+      color: 'red',
+      width: '30px',
+      height: 'auto',
+      padding: '20px'
+      }}><img src={Pin} onMouseEnter={someHandler}/>
+    </div>
+      );
+    }
+
+    function someHandler(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
+        return (
+          <div style={{
+              color: 'red',
+              width: '20px',
+              height: '20px',
+              padding: '20px',
+              zIndex: '999'
+            }}>
+s          </div>
+        )
+      };
+
+ export default Marker;
